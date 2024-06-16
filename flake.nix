3ef007380a93b40d0e370820682cd90f4f8ee024
Sandro <sandro.jaeckel@gmail.com>
2024-06-16 23:25:13 +0200

    Avoid requirement for a nixvim fork (#2)

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "A very basic flake";
+  description = "A very basic NeoVim flake";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
@@ -8,21 +8,47 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nixvim = {
-      url = "github:MarcelCoding/nixvim/nixos-24.05";
+      url = "github:nix-community/nixvim/nixos-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.home-manager.follows = "home-manager";
     };
   };
 
-  outputs = { nixvim, ... }: {
-    homeManagerModules = rec {
-      nvim = {
+  outputs = { self, nixvim, ... }: let
+    angularLsp = { pkgs }: {
+      name = "angularls";
+      package = pkgs.callPackage ./pkgs/angular-language-server { };
+    };
+  in {
+    homeManagerModules = {
+      nvim = { config, lib, pkgs, ... }: let
+        mkLsp = import "${nixvim.outPath}/plugins/lsp/language-servers/_mk-lsp.nix" { inherit config lib pkgs; };
+      in {
         imports = [
           nixvim.homeManagerModules.nixvim
+          {
+            programs.nixvim = mkLsp (angularLsp { inherit pkgs; });
+          }
           ./module
         ];
       };
-      default = nvim;
+      default = self.homeManagerModules.nvim;
+    };
+
+    nixosModules = {
+      nvim = { config, lib, pkgs, ... }: let
+        mkLsp = import "${nixvim.outPath}/plugins/lsp/language-servers/_mk-lsp.nix" { inherit config lib pkgs; };
+      in {
+        imports = [
+          nixvim.nixosModules.nixvim
+          {
+            programs.nixvim = mkLsp (angularLsp { inherit pkgs; });
+          }
+          ./module
+        ];
+      };
+
+      default = self.nixosModules.nvim;
     };
   };
 }
---
+++ pkgs/angular-language-server/default.nix
@@ -0,0 +1,18 @@
+{ buildNpmPackage, fetchurl }:
+
+buildNpmPackage rec {
+  name = "@angular/language-server";
+  version = "18.0.0";
+
+  src = fetchurl {
+    url = "https://registry.npmjs.org/@angular/language-server/-/language-server-${version}.tgz";
+    hash = "sha256-sIp9e4J83lrKM1dFPQOMBstTfM1fBOrGQspNMcINUkA=";
+  };
+
+  postPatch = ''
+    ln -s ${./package-lock.json} package-lock.json
+  '';
+
+  npmDepsHash = "sha256-oLe8X6ykFoMOMMYODP6sEK2s1ihhM35sJf3jpOpfasY=";
+  dontNpmBuild = true;
+}
