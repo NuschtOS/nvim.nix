afa2436a64a8222bd83f798df939f0b856b3aff7
Sandro Jäckel <sandro.jaeckel@gmail.com>
2024-07-05 00:42:20 +0200

    treesitter: remove two biggest syntaxes

--- modules/treesitter.nix
+++ modules/treesitter.nix
@@ -1,5 +1,12 @@
 {
-  plugins.treesitter.enable = true;
+  plugins.treesitter = {
+    enable = true;
+    ignoreInstall = [
+      # remove rather big grammars
+      "systemverilog"
+      "verilog"
+    ];
+  };
 
   # Enable automatically closing and renaming HTML tags
   plugins.ts-autotag.enable = true;
