8518f6c5180881e0033b973896658b584c38db77
Sandro Jäckel <sandro.jaeckel@gmail.com>
2024-07-17 14:02:04 +0200

    treesitter: disable packages used to compile grammers

--- modules/treesitter.nix
+++ modules/treesitter.nix
@@ -5,6 +5,11 @@
     enable = true;
     grammarPackages = lib.filter (g: g.pname != "verilog-grammar" && g.pname != "systemverilog-grammar")
       options.plugins.treesitter.grammarPackages.default;
+
+    # we are not installing packages by hand
+    gccPackage = null;
+    nodejsPackage = null;
+    treesitterPackage = null;
   };
 
   # Enable automatically closing and renaming HTML tags
