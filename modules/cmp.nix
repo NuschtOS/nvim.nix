0bd3701a66f022a38c725439509bae6ae43a1d93
SuperSandroBot <sandro.jaeckel+bot@gmail.com>
2024-09-10 07:55:00 +0200

    flake.lock: Update (#47)
    * flake.lock: Update
    
    Flake lock file updates:
    
    • Updated input 'home-manager':
        'github:nix-community/home-manager/c2cd2a52e02f1dfa1c88f95abeb89298d46023be?narHash=sha256-UNky3lJNGQtUEXT2OY8gMxejakSWPTfWKvpFkpFlAfM%3D' (2024-08-23)
      → 'github:nix-community/home-manager/aaebdea769a5c10f1c6e50ebdf5924c1a13f0cda?narHash=sha256-%2BHsjshXpqNiJHLaJaK0JnIicJ/a1NquKcfn4YZ3ILgg%3D' (2024-09-07)
    • Updated input 'nixpkgs':
        'github:NixOS/nixpkgs/71e91c409d1e654808b2621f28a327acfdad8dc2?narHash=sha256-GnR7/ibgIH1vhoy8cYdmXE6iyZqKqFxQSVkFgosBh6w%3D' (2024-08-28)
      → 'github:NixOS/nixpkgs/574d1eac1c200690e27b8eb4e24887f8df7ac27c?narHash=sha256-v3rIhsJBOMLR8e/RNWxr828tB%2BWywYIoajrZKFM%2B0Gg%3D' (2024-09-06)
    • Updated input 'nixvim':
        'github:nix-community/nixvim/caefb266bee301922a4cf4d4564b1b000a0a21c3?narHash=sha256-tjMrSaxGXC7JQkENchdPPxWv4gPbelPwSoPs5A5e0vU%3D' (2024-08-31)
      → 'github:nix-community/nixvim/810eacf5163b16b666ca70b6617c6a85ce412e0a?narHash=sha256-bzJ5iUPaEjSt24fIoQihBGN%2BQ7mye73hd/jbubHhyZA%3D' (2024-09-07)
    
    * flake: fix eval with unstable
    
    * Disable stable CI
    
    * Fix deprecations
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Sandro Jäckel <sandro.jaeckel@gmail.com>

--- modules/cmp.nix
+++ modules/cmp.nix
@@ -157,7 +157,7 @@ in
 
     luasnip = {
       enable = true;
-      extraConfig = {
+      settings = {
         region_check_events = "CursorHold,InsertLeave";
         # those are for removing deleted snippets, also a common problem
         delete_check_events = "TextChanged,InsertEnter";
