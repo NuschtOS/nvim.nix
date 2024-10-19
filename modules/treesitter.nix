be646ea2182ac096aaf810e44ed50e136e6a60cb
Sandro <sandro.jaeckel@gmail.com>
2024-10-19 08:21:50 +0200

    treesitter: drop 24.11 condition (#66)

--- modules/treesitter.nix
+++ modules/treesitter.nix
@@ -6,7 +6,6 @@
     grammarPackages = lib.filter (g: g.pname != "verilog-grammar" && g.pname != "systemverilog-grammar")
       options.plugins.treesitter.grammarPackages.default;
 
-  } // lib.optionalAttrs (lib.versionAtLeast lib.version "24.11pre") {
     # we are not installing packages by hand
     gccPackage = null;
     nodejsPackage = null;
