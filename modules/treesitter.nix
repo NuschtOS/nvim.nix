babcc103f084da6962657d165d4c61ee3c5624e8
Sandro Jäckel <sandro.jaeckel@gmail.com>
2025-04-12 04:36:51 +0200

    treesitter: fix eval on unstable

--- modules/treesitter.nix
+++ modules/treesitter.nix
@@ -1,6 +1,16 @@
 { lib, options, ... }:
 
-{
+let
+  # TODO: drop with 25.05
+  hasDependencies = lib.versionAtLeast lib.version "25.05";
+in
+lib.optionalAttrs hasDependencies {
+  dependencies = {
+    gcc.enable = false;
+    nodejs.enable = false;
+    tree-sitter.enable = false;
+  };
+} // {
   plugins.treesitter = {
     enable = true;
     # exclude some big (above 10 MB) and rarely used languages
@@ -11,12 +21,12 @@
       && g.pname != "verilog-grammar"
     ) options.plugins.treesitter.grammarPackages.default;
 
+    settings.highlight.enable = true;
+  } // lib.optionalAttrs (!hasDependencies) {
     # we are not installing packages by hand
     gccPackage = null;
     nodejsPackage = null;
     treesitterPackage = null;
-
-    settings.highlight.enable = true;
   };
 
   # Enable automatically closing and renaming HTML tags
