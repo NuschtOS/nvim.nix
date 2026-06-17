5334f6eb46956a60fc1367e786fc74999b0ae5a3
Sandro Jäckel <sandro.jaeckel@sap.com>
2026-06-17 15:20:02 +0200

    Remove option that does not exist on darwin

--- modules/darwin.nix
+++ modules/darwin.nix
@@ -2,7 +2,6 @@
 
 {
   programs.nixvim = {
-    defaultEditor = true;
     nixpkgs = {
       config = lib.mkIf (config.nixpkgs.config != null) config.nixpkgs.config;
       overlays = lib.mkIf (config.nixpkgs.overlays != null) config.nixpkgs.overlays;
